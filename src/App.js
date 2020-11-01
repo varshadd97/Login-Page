3fa3b7fb4afb9b762e8dc9a43642a3f28ef9c5a7
varsharani Dharmashale <varshadd97@gmail.com>
2020-11-01 15:47:17 +0530

    chng

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import react, {useState} from "react";
+import "./App.css"
+const App = () => {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ const [name, setName] = useState("");
+
+ const [lastName, setLastName] = useState("");
+
+ const [fullname, setFullName] = useState();
+ 
+ const [lastNameNew, setLastNameNew] = useState();
+
+ 
+ const onSubmit = (event) => {
+   event.preventDefault();
+   setFullName(name);
+   setLastNameNew(lastName);
+ }
+
+ const inputEvent = (event) => {
+   console.log(event.target.value);
+   setName(event.target.value);
+ }
+
+ const inputEventTwo = (event) => {
+   setLastName(event.target.value)
+ }
+
+  return (<>
+        <div className="App">
+        <form onSubmit={onSubmit}>
+        <div>
+         <h1>{fullname} {lastNameNew}</h1>
+          <input 
+          type="text"
+          placeholder="Enter Your Name"
+          onChange={inputEvent}
+          value={name}
+          />
+          <br />
+          <input 
+          type="text"
+          placeholder="Enter Your last Name"
+          onChange={inputEventTwo}
+          value={lastName}
+          />
+          <br />
+          <button type="submit">Submit</button>
+        </div>
+      </form>
     </div>
+    </>
   );
 }
 
